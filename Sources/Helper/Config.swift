ed306c73b2c0fd6aae35a3a5766f9183ad90c112
Karuppasamy Palanikumar <ios@trioangle.com>
2021-12-27 11:37:20 +0530

    initial Configuration added -> Karuppasamy

--- Sources/Helper/Config.swift
+++ Sources/Helper/Config.swift
@@ -7,10 +7,24 @@
 
 import Foundation
 
-public var Helper_RegularFont = ""
+public var Helper_RegularFont = "GoogleSans-Regular"
 
-public var Helper_MediumFont = ""
+public var Helper_MediumFont = "GoogleSans-Medium"
 
-public var Helper_BoldFont = ""
+public var Helper_BoldFont = "GoogleSans-Bold"
+
+public
+func initFonts(_Helper_RegularFont: String,
+               _Helper_MediumFont: String,
+               _Helper_BoldFont : String) {
+    Helper_RegularFont = _Helper_RegularFont
+    Helper_MediumFont = _Helper_MediumFont
+    Helper_BoldFont = _Helper_BoldFont
+}
 
 public var Helper_isRTLLanguage = false
+
+public
+func initRTL(_Helper_isRTLLanguage: Bool) {
+    Helper_isRTLLanguage = _Helper_isRTLLanguage
+}
